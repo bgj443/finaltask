98624a7d66c02c7927ac088bb1bd35fc26c0f492
bgj443 <bgj443@myy.haaga-helia.fi>
2020-12-16 23:15:50 +0200

    testi error

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -22,7 +22,7 @@ export default function Calendar() {
 
    const fill = () => {
        var x;
-       var eventit = [];
+       var events = [];
        for (x = 0; x < trainings.length; x++) {
         let z = {};
         z.title = trainings[x].customer.firstname + ' ' + trainings[x].customer.lastname + ' - ' + trainings[x].activity;
@@ -30,10 +30,11 @@ export default function Calendar() {
         var y = new Date(trainings[x].date);
         y.setMinutes(y.getMinutes() + trainings[x].duration);
         z.end = y;
-        eventit.push(z);
+        events.push(z);
          
             }
-         setEvents(eventit);
+         setEvents(events);
+
         }
 
 
